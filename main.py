81f347f10659c91e8ecbdf9227480bd25bc60873
ClementTarride <clementarride@gmail.com>
2023-11-15 16:50:53 +0100

    Ajout main

--- main.py
+++ main.py
@@ -28,3 +28,24 @@ def sendMissileAt(rowIndex,columnIndex):
         print("La case a déja été touché")
         return False
     
+def main():
+	
+ 	grid = [['' for _ in range(8)] for _ in range(8)]
+  
+	grid[0][1] = 'b'
+	grid[0][2] = 'b'
+	grid[0][3] = 'b'
+
+	grid[3][2] = 'b'
+	grid[3][3] = 'b'
+
+	grid [7][5] = 'b'
+	grid [6][5] = 'b'
+	grid [5][5] = 'b'
+	grid [4][5] = 'b'	
+	affichage(grid)
+	sendMissileAlt(0,5)
+	sleep(2)
+	affichage(grid)
+ 
+main()
